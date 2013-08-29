e011857c4359850e24f32a3ff4a5a409cc23945f
Simon Donne <donnesimon@gmail.com>
2013-08-29 14:30:17 +0200

    Fixed locator tool: now only your following is counted.

--- src/haven/MapView.java
+++ src/haven/MapView.java
@@ -973,8 +973,11 @@ public class MapView extends PView implements DTarget {
                             reposend = new Coord3f(gobpath.tc.x, gobpath.tc.y, reposstart.z);
                         }
                         
-                        Coord3f relloc = reposend.sub(reposstart).div(11.0f);
-                        LocatorTool.setlocation(new Coord((int)relloc.x, (int)relloc.y));
+                        if(gob.id == this.player().id)
+                        {
+                            Coord3f relloc = reposend.sub(reposstart).div(11.0f);
+                            LocatorTool.setlocation(new Coord((int)relloc.x, (int)relloc.y));
+                        }
                     }
                     else if(m instanceof Following)
                     {
