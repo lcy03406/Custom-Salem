b5a6e0fdddf894a597e7e8fe753bfbf5a3620173
Simon Donne <donnesimon@gmail.com>
2014-07-18 21:06:42 +0200

    Reverted the store url forcing, set correctly in the jnlp file as well now - should work.

--- src/haven/Config.java
+++ src/haven/Config.java
@@ -45,7 +45,7 @@ public class Config {
     public static URL resurl = geturl("haven.resurl", "");
     public static URL mapurl = geturl("haven.mapurl", "");
     public static URL screenurl = geturl("haven.screenurl", "http://game.salemthegame.com/mt/ss");
-    public static URL storeurl = forceurl("http://login.salemthegame.com/portal/tostore");
+    public static URL storeurl = geturl("haven.storeurl","http://login.salemthegame.com/portal/tostore");
     public static URL regurl = geturl("haven.regurl", "http://login.salemthegame.com/beta/nregister");
     public static boolean dbtext = getprop("haven.dbtext", "off").equals("on");
     public static boolean bounddb = getprop("haven.bounddb", "off").equals("on");
